01e050664f3230b3dcb92c9fdb4e426747dbef25
Mohamed Gamal <mg8033667@gmail.com>
2024-02-26 13:17:24 -0800

    adding some files.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,4 +10,15 @@ from api.v1.views import app_views
 @app_views.route('/status', strict_slashes=False)
 def returnstuff():
     '''return stuff'''
-    return jsonify(status='OK')
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def my_stuff():
+    '''return json response.'''
+    todos = {'states': State, 'users': User,
+             'amenities': Amenity, 'cities': City,
+             'places': Place, 'reviews': Review}
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonify(todos)
